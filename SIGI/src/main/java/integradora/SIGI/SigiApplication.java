ce86018ee52960ad72a5dc3135758a9808cf427d
christian20233tn137 <20233tn137@utez.edu.mx>
2024-11-04 19:55:57 -0600

    first commit

---
+++ SIGI/src/main/java/integradora/SIGI/SigiApplication.java
@@ -0,0 +1,13 @@
+package integradora.SIGI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SigiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SigiApplication.class, args);
+	}
+
+}
---
+++ SIGI/src/test/java/integradora/SIGI/SigiApplicationTests.java
@@ -0,0 +1,13 @@
+package integradora.SIGI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
