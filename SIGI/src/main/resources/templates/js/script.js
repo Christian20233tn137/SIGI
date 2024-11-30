d03ee2c53bc8d742ce9d7c67e63d7948b3060b56
AngelMUGIWARA <20233tn144@utez.edu.mx>
2024-11-29 20:30:53 -0600

    Subo cambios cambios que hay en mi

--- SIGI/src/main/resources/templates/js/script.js
+++ SIGI/src/main/resources/templates/js/script.js
@@ -17,7 +17,26 @@ closeModal.addEventListener('click', (e) => {
     modal.classList.remove('modal-show'); // Ocultar modal
 });
 
+//Codigo para el buscador 
+ // Función para filtrar la tabla
+ function filterTable() {
+    const searchInput = document.getElementById("searchInput").value.toLowerCase(); // Obtener el valor del input en minúsculas
+    const rows = document.querySelectorAll(".provider-table tbody tr"); // Seleccionar todas las filas del cuerpo de la tabla
 
+    rows.forEach(row => {
+        const cells = row.querySelectorAll("td"); // Obtener todas las celdas de la fila
+        const rowText = Array.from(cells).map(cell => cell.textContent.toLowerCase()).join(" "); // Concatenar el texto de todas las celdas
+
+        if (rowText.includes(searchInput)) {
+            row.style.display = ""; // Mostrar la fila si coincide con la búsqueda
+        } else {
+            row.style.display = "none"; // Ocultar la fila si no coincide
+        }
+    });
+}
+
+// Asignar el evento al input
+document.getElementById("searchInput").addEventListener("input", filterTable);
 
 
 
